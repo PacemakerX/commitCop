2616a373ff0150af055741d5b021e1e7f6ba39aa
Sparsh <sparsh.officialwork@gmail.com>
2025-07-13 15:39:45 +0530

    fix(repo): Add dependency

--- setup.py
+++ setup.py
@@ -3,23 +3,18 @@ from setuptools import setup, find_packages
 setup(
     name="ccop",
     version="0.1.0",
-    description="Commit message enforcer for Git",
+    description="Git commit message enforcer",
     author="Sparsh",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
+    include_package_data=True,
     install_requires=[
-        "click",
-        # Add any other dependencies you use
+        "click>=8.0.0",
+        "PyYAML>=6.0",
     ],
     entry_points={
         "console_scripts": [
-            "ccop=ccop.main:cli",  # maps `ccop` command to your CLI entry point
-        ]
+            "ccop=ccop.main:cli",
+        ],
     },
-    include_package_data=True,
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.7',
 )
